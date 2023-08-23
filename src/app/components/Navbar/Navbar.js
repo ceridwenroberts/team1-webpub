d96fb9dc903d31405a5f127c868a55aa8dafa527
AnnicaB <annicablixt@outlook.com>
2023-08-23 13:44:23 +0200

    hotfix of navbar and footer

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,1 +1,32 @@
-//dummie
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <span>Home</span>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <span>Gallery</span>
+          </Link>
+        </li>
+        <li>
+          <Link href="/services">
+            <span>About us</span>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <span>Contact</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
