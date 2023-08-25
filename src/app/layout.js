50e825b8c06ad714d6c5b5a3b003eeedc6b0e012
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-08-25 14:07:27 +0200

    linked component footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Montserrat } from 'next/font/google'
 import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 
 const montserrat = Montserrat({ subsets: ['latin'] })
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         </header>
         {children}
         <footer>
+          <Footer />
         </footer>
         </body>
     </html>
